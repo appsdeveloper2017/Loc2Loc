add85b7cbb5e3a18f13d7fba909b0dd46947d7b6
Alex Fernandez <alexfernandez@MacBook-Pro-de-Alex.local>
2018-01-31 23:30:00 +0100

    Cambio de splash.

--- app/src/main/java/com/appdesigndm/loc2loc/SplashScreen.java
+++ app/src/main/java/com/appdesigndm/loc2loc/SplashScreen.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private TextView titulo_splash;
+    private TextView subtitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        titulo_splash = (TextView)findViewById(R.id.titulo_splash);
+        subtitulo = (TextView)findViewById(R.id.subtitulo);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
