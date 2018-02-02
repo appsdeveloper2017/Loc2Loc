e891c6f414d48c105d83305acffa2b74acac726b
David Mendaño <david.mendano@movetia.com>
2018-02-02 15:06:04 +0100

    Animación icono splash, se añaden variables a dimens, colors, ...

--- app/src/main/java/com/appdesigndm/loc2loc/SplashScreen.java
+++ app/src/main/java/com/appdesigndm/loc2loc/SplashScreen.java
@@ -1,27 +1,58 @@
 package com.appdesigndm.loc2loc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
 
-private TextView titulo_screen;
-private ImageView imagen_splash;
+    // UI references
+    private ImageView imageSplash;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mContext = this;
 
+        init();
+        animateOnCreateViews();
 
-        titulo_screen = (TextView)findViewById(R.id.titulo_splash);
-        imagen_splash = (ImageView)findViewById(R.id.imagen_splash);
+    }
 
+    private void init() {
+        imageSplash = (ImageView) findViewById(R.id.imagen_splash);
+    }
 
+    private void animateOnCreateViews() {
+        Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.animation_splash_icon);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                splashScrennDelay();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        imageSplash.startAnimation(animation);
+    }
+
+    private void splashScrennDelay() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -30,6 +61,7 @@ private ImageView imagen_splash;
                 startActivity(intent);
 
             }
-        },2000);
+        }, 1500);
+
     }
 }
