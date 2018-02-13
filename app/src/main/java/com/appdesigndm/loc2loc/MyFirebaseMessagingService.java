f08f86c2aaff233086ec07419fe8e0b66aaea847
Alex Fernandez <alexfernandez@MacBook-Pro-de-Alex.local>
2018-02-13 18:20:29 +0100

    Se suben cambios, las notificaciones simples funcionan pero falta corregir un método que esta deprecate, también hay que crear un panel para mostrar la información completa.

---
+++ app/src/main/java/com/appdesigndm/loc2loc/MyFirebaseMessagingService.java
@@ -0,0 +1,59 @@
+package com.appdesigndm.loc2loc;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by alexfernandez on 13/2/18.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    public static final  String TAG = "NOTICIAS";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String from = remoteMessage.getFrom();
+        Log.d(TAG, "Recibido de..." + from);
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Notificación" + remoteMessage.getNotification().getBody());
+
+            mostarNotificacion(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+    }
+
+    private void mostarNotificacion(String title, String body) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_ONE_SHOT);
+
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
+
