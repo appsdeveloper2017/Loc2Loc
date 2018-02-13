21be36c0fbf5c186269de4e5ec77a7bc2fe8025b
Alex Fernandez <alexfernandez@MacBook-Pro-de-Alex.local>
2018-02-13 22:41:04 +0100

    Se preparan las notificaciones para funcionar con Oreo y versiones anteriores.

--- app/src/main/java/com/appdesigndm/loc2loc/MyFirebaseMessagingService.java
+++ app/src/main/java/com/appdesigndm/loc2loc/MyFirebaseMessagingService.java
@@ -1,15 +1,21 @@
 package com.appdesigndm.loc2loc;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Date;
 
 /**
  * Created by alexfernandez on 13/2/18.
@@ -39,21 +45,59 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private void mostarNotificacion(String title, String body) {
 
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_ONE_SHOT);
+        Intent intent1 = new Intent(this, MainActivity.class);
+        intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
 
+        PendingIntent pendingIntent1 = PendingIntent.getActivity(this, 0, intent1,
+                PendingIntent.FLAG_ONE_SHOT);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(title)
-                .setContentText(body)
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent);
+
+        //int icon = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? R.mipmap.ic_notification_lollypop : R.mipmap.ic_launcher;
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notificationBuilder.build());
+
+        int m = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            String channelId = "Loc2Loc";
+
+
+            NotificationChannel channel = new NotificationChannel(channelId, title, NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription(body);
+            channel.enableLights(true);
+            channel.setLightColor(Color.BLUE);
+            channel.setShowBadge(true);
+            notificationManager.createNotificationChannel(channel);
+
+            Notification notification = new Notification.Builder(this, channelId)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    //.setSmallIcon(icon)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent1)
+                    .build();
+            notificationManager.notify(m, notification);
+
+        } else {
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager1 = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(0, notificationBuilder.build());
+        }
     }
 }
+
 
